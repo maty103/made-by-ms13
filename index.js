ae3612e1d9580e7336ae9d818f1637b70913e34f
maty103 <141565050+maty103@users.noreply.github.com>
2024-04-11 20:01:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to je moeder","Watching porno"];
 
 
 let currentIndex = 0;
